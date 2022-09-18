eb8966d40c894af62796392de413d24eca7d1556
Dell <Dell@DESKTOP-T8VHU5N>
2022-09-18 22:20:24 +0530

    Add project files.

---
+++ Mathmatics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefAndOut
+{
+    internal class Mathmatics
+    {
+        public int Calculation(int num1, int num2, out int Sum, out int Multiply)
+        {
+            Sum = num1 + num2;
+            Multiply = num1 * num2;
+            return Sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RefAndOut;
+
+Mathmatics mathmatics = new();
+int Sum = 0; 
+int Multiply = 0;
+int result = mathmatics.Calculation(10, 20, out Sum, out Multiply);
+Console.WriteLine(result);  //Sum
+Console.WriteLine(Multiply);  //Multiply
+Console.ReadLine();
