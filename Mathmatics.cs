f47d308bd095df644d77585addedcd42884d93bc
Rohit Joshi <rj.rohitjoshi@hotmail.com>
2022-09-24 19:37:03 +0530

    Out Variables Modifiy

--- Mathmatics.cs
+++ Mathmatics.cs
@@ -8,9 +8,15 @@ namespace RefAndOut
 {
     internal class Mathmatics
     {
-        public int Calculation(int num1, int num2, out int Sum, out int Multiply)
+        public int Calculation(int num1, int num2, out int Sum, ref int Multiply)
         {
-            Sum = num1 + num2;
+            /*In Case of Out Variables, We need to initialize it,
+             *inside the callee method, Whatever the request came from the caller it is
+             *discarded, did some manulipation and change Out variable, 
+             *pass it to the caller by out reference.
+            */
+            Sum = 10;
+            Sum = num1 + num2 + Sum;
             Multiply = num1 * num2;
             return Sum;
         }
--- Program.cs
+++ Program.cs
@@ -2,10 +2,9 @@
 
 using RefAndOut;
 
-Mathmatics mathmatics = new();
 int Sum = 0; 
 int Multiply = 0;
-int result = mathmatics.Calculation(10, 20, out Sum, out Multiply);
+int result = new Mathmatics().Calculation(10, 20, out Sum, ref Multiply);
 Console.WriteLine(result);  //Sum
 Console.WriteLine(Multiply);  //Multiply
 Console.ReadLine();
